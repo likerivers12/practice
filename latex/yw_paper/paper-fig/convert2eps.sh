b6a70cc5a4d14d2e9ec8424b2eebdd4e2747ce64
likerivers12 <likerivers12@gmail.com>
2011-01-12 19:43:39 +0900

    paper latex

---
+++ latex/yw_paper/paper-fig/convert2eps.sh
@@ -0,0 +1,86 @@
+# convert fig10-1.bmp fig10-1.eps
+# convert fig10-2.bmp fig10-2.eps
+# convert fig10-3.bmp fig10-3.eps
+
+# convert fig20-1.bmp fig10-1.eps
+# convert fig20-2.bmp fig20-2.eps
+# convert fig20-3.bmp fig30-3.eps
+
+# convert fig30-1.bmp fig30-1.eps
+# convert fig30-2.bmp fig30-2.eps
+
+# convert fig40-1.bmp fig40-1.eps
+# convert fig40-2.bmp fig40-2.eps
+
+# convert fig50.bmp fig50.eps
+
+# convert fig60-.bmp fig60.eps
+
+# convert fig70-1.bmp fig70-1.eps
+# convert fig70-2.bmp fig70-2.eps
+
+# convert fig80-1.bmp fig80-1.eps
+# convert fig80-2.bmp fig80-2.eps
+
+# convert fig90-a.bmp fig90-a.eps
+# convert fig90-b.bmp fig90-b.eps
+# convert fig90-c.bmp fig90-c.eps
+# convert fig90-d.bmp fig90-d.eps
+# convert fig90-e.bmp fig90-e.eps
+# convert fig90-f.bmp fig90-f.eps
+# convert fig90-g.bmp fig90-g.eps
+# convert fig90-h.bmp fig90-h.eps
+
+# convert fig100-1.bmp fig100-1.eps
+# convert fig100-2.bmp fig100-2.eps
+# convert fig100-3.bmp fig100-3.eps
+# convert fig100-4.bmp fig100-4.eps
+# convert fig100-5.bmp fig100-5.eps
+# convert fig100-6.bmp fig100-6.eps
+# convert fig100-7.bmp fig100-7.eps
+# convert fig100-8.bmp fig100-8.eps
+# convert fig100-9.bmp fig100-9.eps
+# convert fig100-10.bmp fig100-10.eps
+# convert fig100-11.bmp fig100-11.eps
+# convert fig100-12.bmp fig100-12.eps
+
+
+# convert fig110-1.bmp fig110-1.eps
+# convert fig110-2.bmp fig110-2.eps
+# convert fig110-3.bmp fig110-3.eps
+# convert fig110-4.bmp fig110-4.eps
+# convert fig110-5.bmp fig110-5.eps
+# convert fig110-6.bmp fig110-6.eps
+# convert fig110-7.bmp fig110-7.eps
+# convert fig110-8.bmp fig110-8.eps
+# convert fig110-9.bmp fig110-9.eps
+# convert fig110-10.bmp fig110-10.eps
+# convert fig110-11.bmp fig110-11.eps
+# convert fig110-12.bmp fig110-12.eps
+
+
+# convert fig120-1.bmp fig120-1.eps
+# convert fig120-2.bmp fig120-2.eps
+# convert fig120-3.bmp fig120-3.eps
+# convert fig120-4.bmp fig120-4.eps
+# convert fig120-5.bmp fig120-5.eps
+# convert fig120-6.bmp fig120-6.eps
+# convert fig120-7.bmp fig120-7.eps
+# convert fig120-8.bmp fig120-8.eps
+# convert fig120-9.bmp fig120-9.eps
+# convert fig120-10.bmp fig120-10.eps
+# convert fig120-11.bmp fig120-11.eps
+# convert fig120-12.bmp fig120-12.eps
+
+# convert fig130-1.bmp fig130-1.eps
+# convert fig130-2.bmp fig130-2.eps
+# convert fig130-3.bmp fig130-3.eps
+# convert fig130-4.bmp fig130-4.eps
+# convert fig130-5.bmp fig130-5.eps
+# convert fig130-6.bmp fig130-6.eps
+# convert fig130-7.bmp fig130-7.eps
+# convert fig130-8.bmp fig130-8.eps
+# convert fig130-9.bmp fig130-9.eps
+# convert fig130-10.bmp fig130-10.eps
+# convert fig130-11.bmp fig130-11.eps
+# convert fig130-12.bmp fig130-12.eps
