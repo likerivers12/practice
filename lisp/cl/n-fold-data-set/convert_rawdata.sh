d5d506c397b9c1339cfeebee69f19e4a1853326a
dt-cygwin <likerivers12@gmail.com>
2011-01-15 17:01:55 +0900

    convert all shell script

---
+++ lisp/cl/n-fold-data-set/convert_rawdata.sh
@@ -0,0 +1,9 @@
+./raw2libsvm.sh
+
+./split_libsvm.sh
+
+./raw2csv.sh
+
+./raw2mlab.sh
+
+./split_mlab.sh
---
+++ lisp/cl/n-fold-data-set/convert_rawdata_2.sh
@@ -0,0 +1,9 @@
+./raw2libsvm_2.sh
+
+./split_libsvm_2.sh
+
+./raw2csv_2.sh
+
+./raw2mlab_2.sh
+
+./split_mlab_2.sh
